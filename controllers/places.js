7aa90dde219128a265c86fd14562441b833771bb
Luis Pimentel <luispimentel@Luiss-MBP.lan>
2023-05-02 07:03:21 -0400

    added places view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,25 @@
 const router = require('express').Router()
 
+// GET /places
 router.get('/', (req, res) => {
-    res.send('Places index page')
-})
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+    res.render('index', { places })
+
+  })
+  
 
 router.post('/', (req, res) => {
     res.send('Create new Place')
