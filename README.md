0362c62498fc94fa0f09c738e47df892592d24b2
Luis Pimentel <luispimentel@Luiss-MBP.lan>
2023-05-01 22:25:14 -0400

    added a route table to the README file

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Project REST-Rant
 
 REST-Rant is an app where users can review restaurants.
+
+# Routes
+| Method | Path | Purpose |
+| :---: | :---: | :--- |
+| GET | / | Home page |ß
+| GET | /places | Places index page |
+| POST | /places | Create new places |
+| GET | /places/new | Form page for creating a new place |
+| GET | /places/:id | Details about a particular place |
+| PUT | /places/:id | Update a particular place |
+| DELETE | /places/:id/edit | Delete a particular place |
+| POST | /places/:id | Create a rant (comment) about a particular place |
+| DELETE | /places/:id/rant/:rantid | Delete a rant(comment)about a particular place |
+| GET | * | 404 page (matches any route not defined above) |
--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,35 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('Get/places')
+    res.send('Places index page')
 })
+
+router.post('/', (req, res) => {
+    res.send('Create new Place')
+})
+router.get('/new', (req, res) => {
+    res.send('Form page for creating a new place')
+})
+router.get('/:id', (req, res) => {
+    res.send('Details about a particular place')
+})
+router.put('/:id', (req, res) => {
+    res.send('Update a particular place')
+})
+
+router.get('/places/:id/edit', (req, res) => {
+    res.send('Form page for editing an existing place')
+})
+router.delete('/:id', (req, res) => {
+    res.send('Delete a particular place')
+})
+
+router.post('/:id/rant', (req, res) => {
+    res.send('Create a rant (comment) about a particular place')
+})
+router.delete('/:id/rant/:rantid', (req, res) => {
+    res.send('Delete a rant (comment) about a particular place')
+})
+
 
 module.exports = router
