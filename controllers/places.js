4a19e51869ebf754e5a29acff58dacf73db17386
Luis Pimentel <luispimentel@Luiss-MBP.lan>
2023-05-14 09:35:55 -0400

    debugging edit

--- controllers/places.js
+++ controllers/places.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   
 //  POST for new places
 router.post('/', (req, res) => {
-    console.log(req.body)
+    
     if (!req.body.pic) {
       // Default image if one is not provided
       req.body.pic = 'http://placekitten.com/400/400'
@@ -42,13 +42,48 @@ router.get('/:id', (req, res) => {
     }
   })
   
-router.put('/:id', (req, res) => {
-    res.send('Update a particular place')
-})
+  router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+  
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+  })
+  
+  
 
 router.get('/:id/edit', (req, res) => {
-    res.render('editpage')
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
 })
+
 
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
